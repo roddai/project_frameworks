45c94bc8c36c85d46f8e9ded4c20d1ee75fa7c2e
Rodrigo <roddai@hotmail.com>
2024-04-30 19:45:43 -0300

    ajusta propriedade frameworksFE e o código HTML

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let data = {
   titulo: 'Principais Frameworks Front-End',
-  frameworksFE: ["MaterializeCSS", "Bulma", "Tailwind CSS", "Bootstrap", "Angular", "Vue.js", "Ember.js", "Svelte"]
+  frameworksFE: [
+    { nome: "MaterializeCSS", classBtn: "btn btn-primary" },
+    { nome: "Bulma", classBtn: "btn btn-secondary" },
+    { nome: "Tailwind CSS", classBtn: "btn btn-success" },
+    { nome: "Bootstrap", classBtn: "btn btn-danger" },
+    { nome: "Angular", classBtn: "btn btn-warning" },
+    { nome: "Vue.js", classBtn: "btn btn-info" },
+    { nome: "Ember.js", classBtn: "btn btn-light" },
+    { nome: "Svelte", classBtn: "btn btn-dark" }]
 }
 
 new Vue({

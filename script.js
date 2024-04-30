eafbf00e43bbdeb65199b55635b688990dfb0a79
Rodrigo <roddai@hotmail.com>
2024-04-30 19:31:53 -0300

    adiciona conteúdo no arquivo JS

---
+++ script.js
@@ -0,0 +1,9 @@
+let data = {
+  titulo: 'Principais Frameworks Front-End',
+  frameworksFE: ["MaterializeCSS", "Bulma", "Tailwind CSS", "Bootstrap", "Angular", "Vue.js", "Ember.js", "Svelte"];
+}
+
+new Vue({
+  el: "#app",
+  data: data,
+});

7432d1cbf3731e90de2476fade37914730a2175f
Rodrigo <roddai@hotmail.com>
2024-05-30 14:34:38 -0300

    escrita do código do arquivo index.js - parte 2

--- index.js
+++ index.js
@@ -30,3 +30,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Inicia o servidor
+app.listen(port, () => {
+  console.log("Aplicação rodando na porta 3000");
+});
+
+// Configuração de rotas
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/auth', function (request, response) {
+  let username = request.body.username;
+  let password = request.body.pswd;
+
+  if (username && password) {
+    connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function (error, results) {
+      if (error) throw error;
+      if (results.length > 0) {
+        request.session.logged = true;
+        request.session.username = username;
+        response.redirect('/home');
+      } else {
+        response.send('Usuário ou senha incorreto(a)!');
+      }
+      response.end();
+    });
+  } else {
+    response.send('Digite seu usuário e senha!');
+    response.end();
+  }
+});
+
+app.get('/home', function (request, response) {
+  if (request.session.logged) {
+    response.send('Olá, ' + request.session.username + '!');
+  } else {
+    response.send('Faça o seu login!');
+  }
+  response.end();
+});
